1acee25617ea3a5c81083725831fd284938368dc
Asante Adarkwa Usman <asante.au96@gmail.com>
2021-11-09 16:06:02 +0000

    implemented splashscreen

---
+++ hive_academy/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hive_academy/home_view.dart';
+import 'package:hive_academy/views/auth/onboarding_view.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hive Academy',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: SplashScreenView(
+        duration: 6000,
+        imageSize: 400,
+        navigateRoute: const OnboardingView(),
+        imageSrc: "assets/images/hiveLogo.png",
+        text: 'powered by KUMASI HIVE',
+        textType: TextType.NormalText,
+        textStyle: const TextStyle(
+          fontSize: 15.0,
+          color: Color.fromRGBO(254, 156, 4, 1),
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
