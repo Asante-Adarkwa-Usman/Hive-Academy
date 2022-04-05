07e1dad8475e8b5c06d3f76798178b8de16a2f80
Asante-Adarkwa-Usman <asante.au96@gmail.com>
2022-04-05 15:52:13 +0000

    Added community screen

--- hive_academy/lib/views/main/course/course_view.dart
+++ hive_academy/lib/views/main/course/course_view.dart
@@ -19,75 +19,80 @@ class CourseView extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(children: <Widget>[
-          Container(
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.only(top: 20, left: 20),
-            child: const Text(
-              'My Courses',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  color: Color.fromRGBO(183, 183, 183, 1)),
-            ),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Container(
-              alignment: Alignment.centerLeft,
-              margin: const EdgeInsets.only(left: 20),
-              child: const Text('Find all courses you own here')),
-          const SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.9,
-            child: TextField(
-              decoration: InputDecoration(
-                //filled: true,
-                floatingLabelBehavior: FloatingLabelBehavior.never,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(top: 20, left: 20),
+                  child: const Text(
+                    'My Courses',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromRGBO(183, 183, 183, 1)),
+                  ),
                 ),
-                labelText: 'Search',
-                labelStyle: const TextStyle(
-                  color: Color.fromRGBO(183, 183, 183, 1),
+                const SizedBox(
+                  height: 40,
                 ),
-                suffixIcon: Icon(
-                  Icons.search,
-                  size: 30,
-                  color: Theme.of(context).primaryColorDark,
+                Container(
+                    alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.only(left: 20),
+                    child: const Text('Find all courses you own here')),
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Container(
-            margin: const EdgeInsets.fromLTRB(15, 25, 15, 0),
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: GridView.builder(
-              itemCount: 8,
-              itemBuilder: (context, index) => const MyCourseCardView(
-                  onTap: null,
-                  completionPercentage: '25',
-                  courseTitle: 'Game Development With Unity'),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.92,
-                crossAxisSpacing: 12,
-                mainAxisSpacing: 12,
-              ),
-            ),
-          )
-          //  SizedBox(
-          //     height: 20,
-          //   ),
-          // const SearchErrorWidget(),
-        ]),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      //filled: true,
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      labelText: 'Search',
+                      labelStyle: const TextStyle(
+                        color: Color.fromRGBO(183, 183, 183, 1),
+                      ),
+                      suffixIcon: Icon(
+                        Icons.search,
+                        size: 30,
+                        color: Theme.of(context).primaryColorDark,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 30,
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(15, 25, 15, 0),
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: GridView.builder(
+                    itemCount: 8,
+                    itemBuilder: (context, index) => const MyCourseCardView(
+                        onTap: null,
+                        completionPercentage: '25',
+                        courseTitle: 'Game Development With Unity'),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.92,
+                      crossAxisSpacing: 12,
+                      mainAxisSpacing: 12,
+                    ),
+                  ),
+                )
+                //  SizedBox(
+                //     height: 20,
+                //   ),
+                // const SearchErrorWidget(),
+              ]),
+        ),
       )),
     )));
   }
--- hive_academy/lib/views/main/payment/payment_view.dart
+++ hive_academy/lib/views/main/payment/payment_view.dart
@@ -1,13 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:hive_academy/custom_widgets/community_card.dart';
 
-class PaymentView extends StatelessWidget {
+class PaymentView extends StatefulWidget {
   const PaymentView({Key? key}) : super(key: key);
 
   @override
+  State<PaymentView> createState() => _PaymentViewState();
+}
+
+class _PaymentViewState extends State<PaymentView> {
+  int _current = 0;
+  final List<Widget> _carouselTextList = const [
+    Text(
+      'Chat with instructors and \n Engage with the Hive community',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 13,
+        fontWeight: FontWeight.w600,
+        color: Colors.white,
+      ),
+    ),
+    Text(
+      'Chat with instructors and \n Engage with the Hive community',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 13,
+        fontWeight: FontWeight.w600,
+        color: Colors.white,
+      ),
+    ),
+    Text(
+      'Chat with instructors and \n Engage with the Hive community',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 13,
+        fontWeight: FontWeight.w600,
+        color: Colors.white,
+      ),
+    ),
+  ];
+  final CarouselController _carouselController = CarouselController();
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Payment screen'),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/Profile.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(children: [
+            Column(
+              children: [
+                Container(
+                    alignment: Alignment.topLeft,
+                    margin: const EdgeInsets.only(top: 15, left: 20),
+                    child: const Text(
+                      'Community',
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w700,
+                          color: Color.fromRGBO(183, 183, 183, 1)),
+                    )),
+                Image.asset(
+                  'assets/images/community.png',
+                  width: MediaQuery.of(context).size.width * 0.45,
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CarouselSlider(
+                  carouselController: _carouselController,
+                  items: _carouselTextList,
+                  options: CarouselOptions(
+                      enlargeCenterPage: true,
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      autoPlay: true,
+                      autoPlayInterval: const Duration(seconds: 7),
+                      autoPlayAnimationDuration:
+                          const Duration(milliseconds: 900),
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _current = index;
+                        });
+                      }),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _carouselTextList.map((e) {
+                    int index = _carouselTextList.indexOf(e);
+                    return Container(
+                      width: 12,
+                      height: 12,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 2.0),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _current == index
+                              ? const Color.fromRGBO(254, 156, 4, 1)
+                              : const Color.fromRGBO(0, 0, 0, .2)),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const CommunityCard(
+                      onTap: null,
+                      title: 'Chat',
+                      cardColor: Colors.black,
+                      imageSrc: 'assets/images/chat.png',
+                    ),
+                    CommunityCard(
+                      onTap: null,
+                      title: 'Forum',
+                      cardColor: Theme.of(context).primaryColorDark,
+                      imageSrc: 'assets/images/forum.png',
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const CommunityCard(
+                  onTap: null,
+                  title: 'Market Place',
+                  cardColor: Color.fromRGBO(167, 167, 167, 1),
+                  imageSrc: 'assets/images/market.png',
+                ),
+              ],
+            ),
+            //The Carousel
+
+            // const SizedBox(height: 80),
+          ]),
+        ),
       ),
     );
   }
