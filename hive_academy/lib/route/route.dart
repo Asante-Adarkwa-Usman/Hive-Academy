07b47459f6d2c409224bdae5f74c81cb2bff82e8
Asante-Adarkwa-Usman <asante.au96@gmail.com>
2022-06-16 16:40:38 +0000

    Added CourseDetailsPage to view course details

--- hive_academy/lib/route/route.dart
+++ hive_academy/lib/route/route.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_academy/views/main/course/course_details/course_details.dart';
 
 import 'package:hive_academy/views/main/course/course_view.dart';
 import 'package:hive_academy/views/main/community/community_view.dart';
@@ -12,7 +13,6 @@ import 'package:hive_academy/views/auth/login_view.dart';
 import 'package:hive_academy/views/auth/onboarding_view.dart';
 import 'package:hive_academy/views/auth/register_view.dart';
 import 'package:hive_academy/views/main/home/home_view.dart';
-import 'package:hive_academy/views/main/course/mainCourseView/main_course_view.dart';
 
 // Route Names
 const String loginPage = '/login';
@@ -20,7 +20,7 @@ const String registerPage = '/register';
 const String onboardingPage = '/onboarding';
 const String homePage = '/home';
 const String coursePage = '/course';
-const String mainCoursePage = '/mainCourse';
+const String courseDetailsPage = '/courseDetails';
 const String communityPage = '/community';
 const String marketPage = '/market';
 const String chatPage = '/chat';
@@ -42,8 +42,11 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => HomeView());
     case coursePage:
       return MaterialPageRoute(builder: (context) => const CourseView());
-    case mainCoursePage:
-      return MaterialPageRoute(builder: (context) => const MainCourseView());
+    case courseDetailsPage:
+      return MaterialPageRoute(
+          builder: (context) => const CourseDetails(
+                id: '',
+              ));
     case communityPage:
       return MaterialPageRoute(builder: (context) => const CommunityView());
     case marketPage:
